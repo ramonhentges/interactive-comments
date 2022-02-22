35008075d232fbc09bf0c99c23c1032df4885a63
Ramon Hentges <r.hentges@hotmail.com>
2022-02-22 14:04:27 -0300

    feat: rate comment component

---
+++ src/components/RateComment/index.tsx
@@ -0,0 +1,27 @@
+import Image from 'next/image';
+
+export const RateComment = () => {
+  return (
+    <div className="flex flex-col px-3 py-1 gap-2 bg-lightGray rounded-lg">
+      <button>
+        <Image
+          src="/assets/icon-plus.svg"
+          alt="Add"
+          objectFit="contain"
+          width={'11px'}
+          height={'11px'}
+        />
+      </button>
+      <p className="font-rubik font-semibold text-base text-blue">12</p>
+      <button>
+        <Image
+          src="/assets/icon-minus.svg"
+          alt="Subtract"
+          objectFit="contain"
+          width={'11px'}
+          height={'11px'}
+        />
+      </button>
+    </div>
+  );
+};
